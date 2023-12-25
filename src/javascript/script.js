3dbcba7f63bf52b7ecab1596873819bc87d7f11a
HiwotAddis <addishiwot963@gmail.com>
2023-12-25 20:37:37 +0300

    bootstrap

---
+++ src/javascript/script.js
@@ -0,0 +1,22 @@
+//sending email
+function sendEmail(){
+    (function(){
+      emailjs.init("lOpjEknKPcIQii6_0");
+    })();
+    var serviceID="service_f28h7xp";
+    var templateID="template_higkcws";
+    var params={
+      sendername: document.querySelector("#name".value),
+      senderemail: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector('#message').value
+    };
+    emailjs.send(serviceID,templateID,params)
+    .then(res => {
+      alert('❤️❤️❤️ Thank you,!! ❤️❤️❤️')
+    })
+    .catch();
+  }
+  
+  
+  
